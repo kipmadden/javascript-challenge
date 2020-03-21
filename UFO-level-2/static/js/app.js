02c78e5c5f087e965055aa0cfbeca321a7cc20d8
kipmadden <56366836+kipmadden@users.noreply.github.com>
2020-03-21 01:38:42 -0500

    UFO-level-2 added multiple inputs

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,30 +2,47 @@
 var ufoData = data;
 // console.log(ufoData);
 // Select the button
-var dateButton = d3.select("#filter-btn");
+var button = d3.select("#filter-btn");
 var tbody = d3.select("tbody");
 
-// console.log(dateButton);
+// console.log(button);
+inputIds = ["#datetime","#city","#state","#country","#shape"];
+inputKeys = ["datetime","city","state","country","shape"];
 
-dateButton.on("click", function(){
+// function to return true or false if input element has a value
+function checkInput(id) {
+    var inputElement = d3.select(id);
+    var inputValue = inputElement.property("value");
+    if (inputValue === "" || inputValue === null) {
+        return false;
+    }
+    else{
+        return true;
+    }
+}
 
-    //Select the datetime input element html raw code
-    var inputDateElement = d3.select("#datetime");
+// filter dataset with passed element id, data.js key value and dataset to filter
+function filterData(id,key,dataset) {
+    var inputElement = d3.select(id);
+    
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
 
-    //Get the value of the datetime input element
-    var inputDateValue = inputDateElement.property("value");
+    var filteredData = dataset.filter(sighting => sighting[key] === inputValue);
+    console.log(filteredData);
 
-    console.log("inputDateValue:",inputDateValue);
-    // console.log(ufoData);
+    return filteredData;
+}
 
-    var filterDateData = ufoData.filter(sighting => sighting.datetime === inputDateValue);
+button.on("click", function(){
 
-    console.log(filterDateData);
 
+    var tableData = filterData("#datetime","datetime",ufoData);
+    // console.log(tableData);
     // Clear any rows and cells from prior taby (if any)
     tbody.html("");
 
-    filterDateData.forEach((sighting) => {
+    tableData.forEach((sighting) => {
         var row = tbody.append("tr");
         Object.entries(sighting).forEach(([key, value]) => {
             var cell = row.append("td");
